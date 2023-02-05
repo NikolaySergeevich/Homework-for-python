e4274ee011ce2e74f3da46a425be9381c6cb0384
Nikolay <nik.pavlovskiy.1997@mail.ru>
2023-02-05 15:57:44 +0300

    make objective 20 for hom 3

--- For seminar 3/main.py
+++ For seminar 3/main.py
@@ -1,5 +1,7 @@
 import Objective_16 as Obj_16
 import Objective_18 as Obj_18
+import Objective_20 as Obj_20
+
 
 
 # --------------------16----------
@@ -10,7 +12,13 @@ num_person = Obj_16.Finding_num_in_list(listt)
 Obj_16.Count_num_in_lsit(listt, num_person)
 '''
 # --------------------18----------
+'''
 listt = Obj_18.Creat_List_num()
 print(listt)
 Obj_18.Close_num(listt)
-
+'''
+# -------------------20------------
+'''
+text = input('Введите текст: ')
+Obj_20.Count_ball(text)
+'''
