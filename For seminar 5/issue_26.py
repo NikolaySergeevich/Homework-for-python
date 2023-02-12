9815fb6dbe897ccbbb230a56e722038840a7193c
Nikolay <nik.pavlovskiy.1997@mail.ru>
2023-02-12 23:18:20 +0300

    Make issue 28 and correct issue 26

--- For seminar 5/issue_26.py
+++ For seminar 5/issue_26.py
@@ -1,7 +1,11 @@
-num = int(input('Введите число: '))
-step = int(input('Введите степень: '))
+# Напишите программу, которая на вход принимает
+# два числа A и B, и возводит число А в целую степень B с
+# помощью рекурсии.
+
 
 def rec(n, st):
+    if st == 0:
+        return 1
     if st == 1:
         return n
-    return n * rec(n, st -1)
+    return n * rec(n, st - 1)
--- For seminar 5/main.py
+++ For seminar 5/main.py
@@ -1,3 +1,11 @@
 from issue_26 import *
-
-print(rec(num, step))
+from issue_28 import *
+# --------------- Issue 26
+num = int(input('Введите число: '))
+step = int(input('Введите степень: '))
+print(f'Число {num} в степени {step} будет = {rec(num, step)}')
+print()
+# --------------- Issue 28
+num_1 = int(input('введите первое число: '))
+num_2 = int(input('введите второе число: '))
+print(f'Сумма {num_1} и {num_2} равна {sum(num_1, num_2, cou = 0)}')
