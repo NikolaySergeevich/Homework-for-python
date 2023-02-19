6f2a9b16dc1c1c9ae7f3bad23a12981c2ee73d57
Nikolay <nik.pavlovskiy.1997@mail.ru>
2023-02-19 22:38:47 +0300

    Mare task 36 for hom 7

--- For seminar 7/tasl_36.py
+++ For seminar 7/tasl_36.py
@@ -5,9 +5,32 @@
 # почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
 # ровно два аргумента, как, например, у операции умножения.
 # Ввод: Вывод:
-# print_operation_table(lambda x, y: x * y) 1 2 3 4 5 6
+# print_operation_table(lambda x, y: x * y) 
+#  1 2 3 4 5 6
 #  2 4 6 8 10 12
 #  3 6 9 12 15 18
 #  4 8 12 16 20 24
 #  5 10 15 20 25 30
 #  6 12 18 24 30 36 
+
+num_row = int(input('Введите кол-во строк: '))
+num_col = int(input('Введите кол-во столбцов: '))
+
+operation = lambda x, y: print(f'элемент по номеру строки и столбца = {x*y}')
+
+def print_operation_table(oper, num_r, num_c):
+    for i in range(1, num_r + 1):
+        for j in range(1, num_c + 1):
+            print(i*j, end=' ')
+        print()
+    num_x = int(input('Введите номер строки: '))
+    num_y = int(input('Введите номер столбца: '))
+    oper(num_x, num_y)
+
+print_operation_table(operation,num_row,num_col)
+
+
+
+
+
+
